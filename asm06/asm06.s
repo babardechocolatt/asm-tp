1df38613e46c4c16735abd7c3ddf47c6d4da4646
babar <theo-delabre@hotmail.com>
2025-10-02 20:57:06 +0200

    exercice asm06 (addition)

--- asm06/asm06.s
+++ asm06/asm06.s
@@ -1,87 +1,141 @@
+section .data
+    error_msg db "Error: need 2 arguments", 10
+    error_len equ $ - error_msg
+
+section .bss
+
 section .text
     global _start
 
 _start:
-    ; On récupère les arguments passés au programme (10 et 15)
-    pop rdi                 ; argc (on s'en fiche)
-    pop rdi                 ; nom du programme (on s'en fiche)
-    pop rdi                 ; rdi pointe vers "10"
-    pop rsi                 ; rsi pointe vers "15"
+    ; Vérifier le nombre d'arguments
+    pop rdi                 ; argc
+    cmp rdi, 3              ; Il faut 3 (programme + 2 args)
+    jne .error              ; Sinon erreur
+    
+    pop rdi                 ; nom programme
+    pop rdi                 ; 1er argument
+    pop rsi                 ; 2ème argument
 
-    ; On convertit "10" en nombre 10
+    ; Conversion 1er nombre (avec signe)
     call atoi
-    mov r12, rax           ; r12 = 10
+    mov r12, rax
 
-    ; On convertit "15" en nombre 15
+    ; Conversion 2ème nombre (avec signe)
     mov rdi, rsi
     call atoi
-    mov r13, rax           ; r13 = 15
+    mov r13, rax
 
-    ; On additionne : 10 + 15 = 25
+    ; Addition
     mov rax, r12
-    add rax, r13           ; rax = 25
+    add rax, r13
 
-    ; On affiche le résultat (25)
+    ; Affichage (avec signe si négatif)
     mov rdi, rax
     call print_number
 
-    ; On affiche un retour à la ligne
+    ; Nouvelle ligne
     mov rdi, 10
     call print_char
 
-    ; On quitte le programme
+    ; Exit succès
     mov rax, 60
     xor rdi, rdi
     syscall
 
-; Transforme une chaîne ASCII ("10") en entier (10)
+.error:
+    ; Afficher message d'erreur
+    mov rax, 1
+    mov rdi, 2              ; stderr
+    mov rsi, error_msg
+    mov rdx, error_len
+    syscall
+    
+    ; Exit avec code 1
+    mov rax, 60
+    mov rdi, 1
+    syscall
+
+
+; Convertit string en nombre (gère le signe -)
 atoi:
-    xor rax, rax           ; rax = 0
+    xor rax, rax
     xor rcx, rcx
-.loop_atoi:
-    movzx rcx, byte [rdi]  ; Lit un caractère ('1' puis '0')
-    cmp rcx, 48            ; Si < '0'
-    jl .done_atoi
-    cmp rcx, 57            ; Si > '9'
-    jg .done_atoi
-    sub rcx, 48            ; Convertit ASCII -> chiffre
-    imul rax, rax, 10      ; Multiplie par 10
-    add rax, rcx           ; Ajoute le chiffre
-    inc rdi                ; Caractère suivant
-    jmp .loop_atoi
-.done_atoi:
+    xor r8, r8              ; r8 = flag négatif (0 = positif, 1 = négatif)
+    
+    ; Vérifier si le nombre commence par '-'
+    movzx rcx, byte [rdi]
+    cmp rcx, 45             ; '-' = ASCII 45
+    jne .loop
+    mov r8, 1               ; C'est négatif
+    inc rdi                 ; Passer le '-'
+
+.loop:
+    movzx rcx, byte [rdi]
+    cmp rcx, 48
+    jl .done
+    cmp rcx, 57
+    jg .done
+    sub rcx, 48
+    imul rax, rax, 10
+    add rax, rcx
+    inc rdi
+    jmp .loop
+
+.done:
+    ; Si négatif, inverser le signe
+    test r8, r8
+    jz .positive
+    neg rax                 ; rax = -rax
+
+.positive:
     ret
 
-; Affiche un entier (25 → "25")
+
+; Affiche un nombre (gère les négatifs)
 print_number:
     mov rax, rdi
+    
+    ; Si négatif, afficher '-' et prendre la valeur absolue
+    test rax, rax
+    jns .positive           ; Si positif, continuer
+    
+    push rax
+    mov rdi, 45             ; '-' = ASCII 45
+    call print_char
+    pop rax
+    neg rax                 ; Prendre la valeur absolue
+
+.positive:
     mov rcx, 10
     push 0
     
 .divide:
     xor rdx, rdx
-    div rcx                ; RAX / 10, reste dans RDX
-    add rdx, 48            ; Convertit en ASCII
-    push rdx               ; Empile le chiffre
+    div rcx
+    add rdx, 48
+    push rdx
     test rax, rax
     jnz .divide
     
 .print:
-    pop rdi                ; Dépile chiffre
+    pop rdi
     test rdi, rdi
-    jz .done_print
+    jz .done
     call print_char
     jmp .print
-.done_print:
+    
+.done:
     ret
 
-; Affiche un caractère (dans RDI)
+
+; Affiche un caractère
 print_char:
     push rdi
-    mov rax, 1             ; syscall write
-    mov rdi, 1             ; stdout
-    mov rsi, rsp           ; adresse du caractère
-    mov rdx, 1             ; longueur = 1
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, rsp
+    mov rdx, 1
     syscall
     pop rdi
     ret
